5b7184a37312bd43e8ec2120a13b10af775d5f90
ringsaturn <ringsaturn.me@gmail.com>
2024-09-04 18:57:28 +0800

    debug

--- src/lib.rs
+++ src/lib.rs
@@ -1,33 +1,18 @@
-use axum::{extract::Query, routing::get, Router};
-use reqwest;
+use axum::{extract::Query, http::StatusCode, response::IntoResponse, routing::get, Json, Router};
 use serde::Deserialize;
+use serde::Serialize;
 use tower_service::Service;
 use tzf_rs::gen;
 use tzf_rs::Finder;
 use worker::*;
 
-fn router() -> Router {
-    Router::new().route("/", get(get_tz))
-}
-
 async fn new_finder_via_download() -> Finder {
-    let download_url: &str =
+    let url: &str =
         "https://github.com/ringsaturn/tzf-rel-lite/raw/main/combined-with-oceans.reduce.pb";
-    let resp: reqwest::Response = reqwest::get(download_url).await.unwrap();
-    let file_bytes: axum::body::Bytes = resp.bytes().await.unwrap();
-    let file_bytes: Vec<u8> = file_bytes.to_vec();
+    let request = Request::new(url, Method::Get).unwrap();
+    let mut response = Fetch::Request(request).send().await.unwrap();
+    let file_bytes = response.bytes().await.unwrap();
     Finder::from_pb(gen::Timezones::try_from(file_bytes).unwrap_or_default())
-}
-
-#[event(fetch)]
-async fn fetch(
-    req: HttpRequest,
-    _env: Env,
-    _ctx: Context,
-) -> Result<axum::http::Response<axum::body::Body>> {
-    console_error_panic_hook::set_once();
-
-    Ok(router().call(req).await?)
 }
 
 #[derive(Debug, Deserialize)]
@@ -36,16 +21,33 @@ pub struct Params {
     lat: f64,
 }
 
-pub async fn get_tz(Query(params): Query<Params>) -> String {
+#[derive(Serialize)]
+pub struct GetTZResponse {
+    tz: String,
+}
 
-    // let lng = 116.4074;
-    // let lat = 39.9042;
-    // let tz = "{lng},{lat}""
-    // let tz = format!("{lng},{lat}");
+pub async fn get_tz(Query(params): Query<Params> ) -> impl IntoResponse {
     let lng = params.lng;
     let lat = params.lat;
-    let f = new_finder_via_download().await;
 
+    let f = new_finder_via_download().await;
     let tz = f.get_tz_name(lng, lat);
-    tz.to_string()
+    let tz = tz.to_string();
+
+    (StatusCode::OK, Json(GetTZResponse { tz }))
+}
+
+fn router() -> Router {
+    Router::new().route("/", get(get_tz))
+}
+
+#[event(fetch)]
+pub async fn main(
+    req: HttpRequest,
+    _env: Env,
+    _ctx: Context,
+) -> Result<axum::http::Response<axum::body::Body>> {
+    console_error_panic_hook::set_once();
+
+    Ok(router().call(req).await?)
 }
